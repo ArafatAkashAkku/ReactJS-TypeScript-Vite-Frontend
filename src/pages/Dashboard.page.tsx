575945409a74dd9925bbeb64f97f408e9eaedc32
Md. Arafat Akash <114158075+ArafatAkashAkku@users.noreply.github.com>
2025-06-04 12:48:54 +0600

    Package and app upadted

--- src/pages/Dashboard.page.tsx
+++ src/pages/Dashboard.page.tsx
@@ -2,27 +2,33 @@ import { useEffect } from 'react';
 import { useUser } from '../context/User.context';
 import { useNavigate } from 'react-router-dom';
 import { io } from 'socket.io-client';
+import { isDevelopment } from '../utilities/app.utilities';
 
 const Dashboard = () => {
   const { user, logout } = useUser();
   const navigate = useNavigate();
+  const socketUrl = import.meta.env.VITE_SOCKET_URL;
 
   // socket initialization
   // Note: Ensure that the server is running and accessible at this URL
-  const socket = io('http://localhost:5000', {
+  const socket = io(socketUrl, {
     transports: ['websocket'], // Enforce only websocket
+    autoConnect: false,
   });
 
   useEffect(() => {
+    if (!socket.connected) {
+      socket.connect();
+    }
     socket.on('connect', () => {
-      console.log('Connected to server:', socket.id);
+      if (isDevelopment) console.log('Connected to server:', socket.id);
     });
   }, []);
 
   const handleLogout = () => {
     logout();
     socket.disconnect();
-    console.log('Disconnected from server:', socket.id);
+    if (isDevelopment) console.log('Disconnected from server');
     navigate('/login');
   };
 
